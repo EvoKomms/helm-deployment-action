c60d2fad6b0c82ab9655a5bb9282bb754344d7b2
Sudipto Sarkar <sudiptosarkar@visioplanet.org>
2026-02-20 19:02:42 +0530

    Added support for direct helm variables (#116)
    * Added support for direct helm variables
    * Upgraded all upgradable dependencies
    * Updated action inputs for the direct helm variable prefix
    * Updated documentation for direct helm variable usage
    * Added support for multi-chart direct helm variables

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@ const environment = process.env.DEPLOYMENT_ENVIRONMENT;
 const helmConfigMapVariableName = process.env.HELM_CONFIG_MAP_VARIABLE_NAME;
 const helmSecretVariableName = process.env.HELM_SECRET_VARIABLE_NAME;
 const helmEnvVarVariableName = process.env.HELM_ENV_VAR_VARIABLE_NAME;
+const directHelmVariablePrefix = process.env.DIRECT_VARIABLE_PREFIX;
 const tag = process.env.TAG;
 
 let helmDeploymentCommand = `helm upgrade ${name} \\
@@ -34,6 +35,7 @@ if (dryRun) {
 
 const helmSecrets: Array<HelmVariableDto> = [];
 const helmConfigMaps: Array<HelmVariableDto> = [];
+const helmVariables: Array<HelmVariableDto> = [];
 const helmEnvVars: Array<HelmEnvVarDto> = [];
 
 if (!helmChartUrl) {
@@ -68,8 +70,15 @@ for (const variableName in variables) {
         if (dryRun) {
           console.log(`Configuring Variable with key: ${variableName}`);
         }
-        helmConfigMaps.push({ key: varValue.key, useFile: varValue.useFile, value: varValue.value });
+        if (variableName.startsWith(`DEPLOYMENT_${prefix ? prefix.concat('_') : ''}${environment ? environment.concat('_') : ''}${directHelmVariablePrefix}_`)) {
+          helmVariables.push({ key: varValue.key, useFile: varValue.useFile, value: varValue.value });
+        } else {
+          helmConfigMaps.push({ key: varValue.key, useFile: varValue.useFile, value: varValue.value });
+        }
         useFile = useFile || varValue.useFile === true;
+      } else if (!varValue.chart && variableName.startsWith(`DEPLOYMENT_${prefix ? prefix.concat('_') : ''}${environment ? environment.concat('_') : ''}${directHelmVariablePrefix}_`)) {
+        // Global Direct Helm Variable
+        helmVariables.push({ key: varValue.key, useFile: varValue.useFile, value: varValue.value });
       }
     } catch (err: unknown) {
       console.error(`Misconfigured Github Variable: ${variableName}. Please correct and re-run.`, err);
@@ -103,6 +112,13 @@ if (useFile) {
     valuesFileContent += `    value: ${getValue(dto)}\n`;
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const helmValuesObject: any = {};
+  for (const dto of helmVariables) {
+    setDeepValue(helmValuesObject, dto.key, getValue(dto));
+  }
+  valuesFileContent += objectToYaml(helmValuesObject);
+
   fs.writeFileSync('.helmValues.yaml', valuesFileContent);
 
   helmDeploymentCommand = helmDeploymentCommand.concat(`-f .helmValues.yaml \\\n  ${helmChartUrl}`);
@@ -124,6 +140,8 @@ if (useFile) {
     helmDeploymentCommand = helmDeploymentCommand.concat(`--set ${helmEnvVarVariableName}[${idx}].name='${dto.name}' \\\n  `);
     helmDeploymentCommand = helmDeploymentCommand.concat(`--set ${helmEnvVarVariableName}[${idx}].value='${getValue(dto)}' \\\n  `);
   }
+
+  for (const dto of helmVariables) helmDeploymentCommand = helmDeploymentCommand.concat(`--set ${dto.key}='${getValue(dto)}' \\\n  `);
 
   if (helmChartVersion) {
     helmDeploymentCommand = helmDeploymentCommand.concat(`--version ${helmChartVersion} \\\n  `);
@@ -162,10 +180,8 @@ if (dryRun) {
 function getValue(dto: HelmVariableDto | HelmEnvVarDto): string {
   if (useFile && 'useFile' in dto && dto.useFile) {
     return `|\n      ${dto.value}`;
-  } else if (useFile) {
-    return escapeGoSpecialChars(dto.value);
   } else {
-    return escapeGoSpecialChars(dto.value);
+    return escapeGoSpecialChars(dto.value, useFile);
   }
 }
 
@@ -174,8 +190,9 @@ function sanitize(err: Error, secret: string) {
   err.stack = err.stack?.replace(escapeGoSpecialChars(secret), '<REDACTED>');
 }
 
-function escapeGoSpecialChars(value: string): string {
-  return value
+function escapeGoSpecialChars(value: string, useFile: boolean = false): string {
+  const retVal = useFile ? value : value.split('\\').join(String.raw`\\`); // Do not escape backslashes if we are using file, as they are interpreted literally in that case.
+  return retVal
     .split(',')
     .join(String.raw`\,`)
     .split('.')
@@ -188,4 +205,72 @@ function escapeGoSpecialChars(value: string): string {
     .join(String.raw`\]`)
     .split('}')
     .join(String.raw`\}`);
+}
+
+type HelmValue = string | Array<HelmValue> | { [key: string]: HelmValue };
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function setDeepValue(obj: any, path: string, value: string) {
+  const parts = path.replace(/\[(\d+)]/g, '.$1').split('.');
+  let current = obj;
+  for (let i = 0; i < parts.length - 1; i++) {
+    const part = parts[i];
+    const nextPart = parts[i + 1];
+    const isNextIndex = /^\d+$/.test(nextPart);
+
+    if (current[part] === undefined) {
+      current[part] = isNextIndex ? [] : {};
+    }
+
+    current = current[part];
+  }
+  const lastPart = parts[parts.length - 1];
+
+  if (/^\d+$/.test(lastPart) && Array.isArray(current)) {
+    current[Number.parseInt(lastPart)] = value;
+  } else {
+    current[lastPart] = value;
+  }
+}
+
+function objectToYaml(obj: HelmValue, indentLevel = 0): string {
+  let yaml = '';
+  const indent = '  '.repeat(indentLevel);
+
+  if (Array.isArray(obj)) {
+    for (const item of obj) {
+      if (typeof item === 'object' && item !== null && !Array.isArray(item)) {
+        const itemObj = item as { [key: string]: HelmValue };
+        const keys = Object.keys(itemObj);
+
+        if (keys.length === 1) {
+          const firstKey = keys[0];
+          const firstVal = itemObj[firstKey];
+
+          if (typeof firstVal === 'object' && firstVal !== null) {
+            yaml += `${indent}- ${firstKey}:\n${objectToYaml(firstVal, indentLevel + 2)}`;
+          } else {
+            yaml += `${indent}- ${firstKey}: ${firstVal}\n`;
+          }
+        } else {
+          yaml += `${indent}-\n${objectToYaml(itemObj, indentLevel + 1)}`;
+        }
+      } else {
+        yaml += `${indent}- ${item}\n`;
+      }
+    }
+  } else if (typeof obj === 'object' && obj !== null) {
+    const objVal = obj as { [key: string]: HelmValue };
+    for (const key in objVal) {
+      if (Object.prototype.hasOwnProperty.call(objVal, key)) {
+        const value = objVal[key];
+        if (typeof value === 'object' && value !== null) {
+          yaml += `${indent}${key}:\n${objectToYaml(value, indentLevel + 1)}`;
+        } else {
+          yaml += `${indent}${key}: ${value}\n`;
+        }
+      }
+    }
+  }
+  return yaml;
 }
